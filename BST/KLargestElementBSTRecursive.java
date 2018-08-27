f94a0649567561dc187799e9a659b71478d4f275
Lee Huey Charn <hueycharn.lee@gmail.com>
2018-08-26 22:55:08 -0700

    qs 14.3 - recursive

---
+++ BST/KLargestElementBSTRecursive.java
@@ -0,0 +1,25 @@
+/* qs 14.3 - recursive - reversed in order */
+
+public List<Integer> KLargestElementBST(Node node, int k)
+{
+	List<Integer> myList = new ArrayList<>();
+
+	recursiveHelper(node, k, myList);
+
+	return myList;
+}
+
+public void recursiveHelper(Node node, int k, List<Integer> list)
+{
+	if(node != null && list.size < k)
+	{
+		recursiveHelper(node.right);
+		if(list.size < k)
+		{
+			list.add(node);
+		}
+
+		recursiveHelper(node.left);
+	}
+}
+
